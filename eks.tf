bcea7929fdabaef7490ed1537bcf49e030d9db85
Daniel Randall <dgrandall@me.com>
2020-01-22 13:59:11 -0700

    eks

--- eks.tf
+++ eks.tf
@@ -1,6 +1,7 @@
 // iam junk for eks cluster
-resource "aws_iam_role" "eks_role" {
-  name = "prd-eks-role"
+resource "aws_iam_role" "eks_cluster" {
+  name = "eks-cluster-role"
+  // permissions_boundary = "arn:aws:iam::586877430255:policy/iamRolePermissionBoundary"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -14,19 +15,45 @@ resource "aws_iam_role" "eks_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+resource "aws_iam_role_policy_attachment" "eks_cluster-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = "${aws_iam_role.eks_role.name}"
+  role       = aws_iam_role.eks_cluster.name
 }
 
-resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
+resource "aws_iam_role_policy_attachment" "eks_cluster-AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = "${aws_iam_role.eks_role.name}"
+  role       = aws_iam_role.eks_cluster.name
 }
+
+// eks cluster
+resource "aws_eks_cluster" "eks" {
+  name     = "eks-cluster"
+  role_arn = aws_iam_role.eks_cluster.arn
+
+  vpc_config {
+    subnet_ids = module.acs.private_subnet_ids
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks_cluster-AmazonEKSServicePolicy,
+  ]
+}
+
+/*
+output "eks_endpoint" {
+  value = aws_eks_cluster.eks.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.eks.certificate_authority.0.data
+}
+*/
 
 // iam junk for eks nodes
-resource "aws_iam_role" "eks_node_role" {
-  name = "eks-node-group"
+resource "aws_iam_role" "eks_node_group" {
+  name = "eks-node-group-role"
+  // permissions_boundary = "arn:aws:iam::586877430255:policy/iamRolePermissionBoundary"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -40,52 +67,26 @@ resource "aws_iam_role" "eks_node_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+resource "aws_iam_role_policy_attachment" "eks_node_group-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.eks_node_role.name
+  role       = aws_iam_role.eks_node_group.name
 }
 
-resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+resource "aws_iam_role_policy_attachment" "eks_node_group-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.eks_node_role.name
+  role       = aws_iam_role.eks_node_group.name
 }
 
-// don't think i need this one
-/*
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+resource "aws_iam_role_policy_attachment" "eks_node_group-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.example.name
-}
-*/
-
-// eks cluster
-resource "aws_eks_cluster" "eks" {
-  name     = "eks"
-  role_arn = aws_iam_role.eks_role.arn
-
-  vpc_config {
-    subnet_ids = module.acs.private_subnet_ids
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.AmazonEKSServicePolicy,
-  ]
-}
-
-output "prd_eks_endpoint" {
-  value = aws_eks_cluster.eks.endpoint
-}
-
-output "prd_kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.eks.certificate_authority.0.data
+  role       = aws_iam_role.eks_node_group.name
 }
 
 // eks nodes
 resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "eks-node-group"
-  node_role_arn   = aws_iam_role.eks_node_role.arn
+  node_role_arn   = aws_iam_role.eks_node_group.arn
   subnet_ids      = module.acs.private_subnet_ids
 
   ami_type       = "AL2_x86_64"
@@ -99,8 +100,8 @@ resource "aws_eks_node_group" "eks_node_group" {
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-    // aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.eks_node_group-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.eks_node_group-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.eks_node_group-AmazonEC2ContainerRegistryReadOnly,
   ]
 }
