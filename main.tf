98d75b25952bfdf9ed5979e0e5cb1a6ebdd27d76
Brayden Winterton <bwinterton@gmail.com>
2020-01-22 10:49:00 -0700

    Change backend to s3

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-storage-586877430255"
+    lock_table = "terraform-state-lock-586877430255"
+    key = "base-account-config.tfstate"
+    region = "us-west-2"
+  }
+}
+
 provider "aws" {
   region = "us-west-2"
 }
