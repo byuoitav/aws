2acbaf7abba5d8a6aef1a094b8099fa48ae97511
Daniel Randall <dgrandall@me.com>
2020-02-10 10:20:31 -0700

    use 1.0 alb

--- alb-ingress-controller.tf
+++ alb-ingress-controller.tf
@@ -1,6 +1,6 @@
 module "alb-ingress-controller" {
   source  = "byuoitav/alb-ingress-controller/kubernetes"
-  version = "0.1.2"
+  version = "1.0.0"
 
   // required
   k8s_cluster_name = aws_eks_cluster.av.name
