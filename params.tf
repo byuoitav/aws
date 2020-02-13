8ede6589b876e79f461d9285946b0065bdbe600f
Daniel Randall <dgrandall@me.com>
2020-02-12 19:40:33 -0700

    change param names

--- params.tf
+++ params.tf
@@ -1,11 +1,13 @@
-resource "aws_ssm_parameter" "eks_cluster_name" {
-  name  = "/eks/default-cluster-name"
-  type  = "String"
-  value = aws_eks_cluster.av.name
+resource "aws_ssm_parameter" "eks_av_cluster_endpoint" {
+  name      = "/eks/av-cluster-endpoint"
+  type      = "String"
+  value     = aws_eks_cluster.av.endpoint
+  overwrite = true
 }
 
-resource "aws_ssm_parameter" "acm_cert_arn" {
-  name  = "/acm/av-cert-arn"
-  type  = "String"
-  value = aws_acm_certificate.av_cert.arn
+resource "aws_ssm_parameter" "acm_av_cert_arn" {
+  name      = "/acm/av-cert-arn"
+  type      = "String"
+  value     = aws_acm_certificate.av_cert.arn
+  overwrite = true
 }
