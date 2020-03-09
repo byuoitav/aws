281cd75ad6c83a93334d886ddb81ab7a8dd67f00
Brayden Winterton <bwinterton@gmail.com>
2020-03-09 16:30:08 -0600

    Add opa to the frey

---
+++ opa.tf
@@ -0,0 +1,19 @@
+module "statefulset_dev" {
+  source = "github.com/byuoitav/terraform//modules/kubernetes-statefulset"
+
+  // required
+  name                 = "opa-dev"
+  image                = "openpolicyagent/opa"
+  image_version        = "latest"
+  container_port       = 8181
+  repo_url             = "https://github.com/byuoitav/aws"
+  storage_mount_path   = "/opt/policies"
+  storage_request_size = "10Gi"
+
+  // optional
+  container_env = {}
+  container_args = [
+    "run", "--server",
+    "--log-level", "debug"
+  ]
+}
